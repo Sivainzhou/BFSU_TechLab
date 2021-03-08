16afa378a20878cc7fd85a9548cedabd535679e5
YihangZhou <80266069+Sivainzhou@users.noreply.github.com>
2021-03-08 20:44:07 +0100

    Add files via upload

---
+++ print_type_input.py
@@ -0,0 +1,35 @@
+# to print an output
+print('Yihang')
+
+# imaginary dog
+print('o----')
+print(' ||||') # Codes are executed line by line
+
+# multiplications on str
+print('*' *10)
+
+# types
+print(type(10)) # int
+print(type(10.0)) # float
+print(type('type')) # str
+print(type(True)) # boolean, and Python is case sensitive
+
+# Exercise 1
+full_name = 'John Smith'
+age = 23
+is_new = True
+
+# input
+name = input('What is your name?')
+print('Hi,', name)
+
+# Exercise 2
+name = input('What is your name?')
+color = input('What is your favorite color?')
+print(name, 'likes', color)
+
+# type transformation
+birth_year = input('Birth year: ')
+age = 2021 - int(birth_year)
+print('You are about', age, 'years old.')
+
